1da7ef04044124a6847575eeef9e34c1e68b70eb
John Hinke <johnh@polymathrobotics.com>
2026-05-08 11:31:22 -0700

    Adding CRC 16 support
    - Calculating CRC on the bytes that are sent/received

--- pstop_c/pstop/src/pstop/checksum.c
+++ pstop_c/pstop/src/pstop/checksum.c
@@ -6,8 +6,22 @@
 
 #include "pstop/checksum.h"
 
+static const uint16_t POLY = 0x1021U;
+
 uint16_t
 checksum_crc16(const uint8_t *data, size_t data_length)
 {
-    return 0U;
+    uint16_t crc = 0xFFFF;
+
+    for(uint16_t i = 0U; i < data_length; ++i) {
+        crc ^= (uint16_t)data[i] << 8U;
+        for(uint16_t j = 0U; j < 8U; ++j) {
+            if(crc & 0x8000U) {
+                crc = (crc << 1U) ^ POLY;
+            } else {
+                crc <<= 1U;
+            }
+        }
+    }
+    return crc;
 }
