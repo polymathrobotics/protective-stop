c35bd952c3c68b18f50ca94a81f651af2d98da57
Ilia Baranov <90713890+iliabaranov@users.noreply.github.com>
2026-07-23 12:22:06 -0700

    Revert "Restructure into firmware monorepo; archive ROS2; add CI + OSS scaffolding (#53)"
    This reverts commit 65273eddd2c416851a3544a9169486d478f86096.

---
+++ protective_stop_node/test/launch.test.py
@@ -0,0 +1,230 @@
+# SPDX-FileCopyrightText: 2026 Polymath Robotics, Inc.
+# SPDX-License-Identifier: Apache-2.0
+# !/usr/bin/env python3
+import time
+import uuid
+
+import launch_testing
+import launch_testing.actions
+import launch_testing.util
+import pytest
+import rclpy
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathSubstitution
+from launch_ros.substitutions import FindPackageShare
+from rcl_interfaces.srv import SetParameters
+
+from protective_stop_msg.srv import ProtectiveStop as ProtectiveStopSrv
+from protective_stop_node.models import PStopRemoteStatusEnum
+from protective_stop_node.test_utils.base_test import BaseTestProtectiveStopNode
+from protective_stop_node.test_utils.helpers import build_pstop_message
+
+machine_uuid = 'machine-uuid'
+TEST_HEARTBEAT_TIMEOUT_S = 0.1
+TEST_DEACTIVATION_TIMEOUT_S = 3.0
+TIMEOUT_PADDING = 0.05
+MAX_PSTOP_COUNT = 3
+
+
+@pytest.mark.launch_test
+def generate_test_description():
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PathSubstitution(FindPackageShare('protective_stop_node')) / 'launch' / 'protective_stop_node.launch.yaml',
+            launch_arguments={
+                'machine_uuid': machine_uuid,
+                'heartbeat_timeout': str(TEST_HEARTBEAT_TIMEOUT_S),
+                'deactivation_timeout': str(TEST_DEACTIVATION_TIMEOUT_S),
+                'max_pstop_count': str(MAX_PSTOP_COUNT),
+            }.items(),
+        ),
+        launch_testing.actions.ReadyToTest(),
+    ])
+
+
+"""
+TODO(troy):
+- put launch test on different domain_id to maintain purity
+"""
+
+
+class TestProtectiveStopNode(BaseTestProtectiveStopNode):
+    def test_activation_lifecycle(self):
+        self._activate()
+
+        self._deactivate()
+
+    def test_err_for_multiple_activations(self):
+        self._activate()
+
+        request = ProtectiveStopSrv.Request()
+        request.recv_uuid = machine_uuid
+        request.sender_uuid = self.uuid
+
+        future = self.activate_client.call_async(request)
+        rclpy.spin_until_future_complete(self.node, future)
+
+        response = future.result()
+        self.assertFalse(response.success)
+
+    def test_err_uuid_mismatch_state_transition_service(self):
+        request = ProtectiveStopSrv.Request()
+        # Set to unknown machine uuid
+        request.recv_uuid = str(uuid.uuid4())
+        request.sender_uuid = self.uuid
+
+        future = self.activate_client.call_async(request)
+        rclpy.spin_until_future_complete(self.node, future)
+
+        response = future.result()
+        self.assertIsNotNone(response, 'No response received from /state_transition_service')
+        self.assertFalse(response.success)
+        self.assertEqual(response.message, 'Unknown receiver uuid')
+        self.assertEqual(response.params.machine_uuid, machine_uuid)
+
+    def test_err_deactivating_before_activating(self):
+        request = ProtectiveStopSrv.Request()
+        request.recv_uuid = machine_uuid
+        request.sender_uuid = self.uuid
+
+        future = self.deactivate_client.call_async(request)
+        rclpy.spin_until_future_complete(self.node, future)
+
+        response = future.result()
+        self.assertIsNotNone(response, 'No response received from /state_transition_service')
+        self.assertFalse(response.success)
+        self.assertEqual(response.message, 'Deactivation failed. UUID not in connected list')
+
+    def test_multiple_activations_deactivations(self):
+        for _ in range(MAX_PSTOP_COUNT):
+            self._activate()
+            self._spin_till_ok()
+            self._deactivate()
+
+    def test_max_connections(self):
+        for _ in range(MAX_PSTOP_COUNT + 1):
+            self._activate(str(uuid.uuid4()))
+            self._deactivate(str(uuid.uuid4()))
+
+    def test_receives_ok_messages(self):
+        self._activate()
+        self._spin_till_ok()
+        self._deactivate()
+
+    def test_presses_pstop(self):
+        self._activate()
+        self._spin_till_ok()
+
+        self.assertFalse(self.pstop_hb_msgs[-1].stop, 'Protective stop bool not set to true')
+
+        PSTOP_PRESSED = True
+
+        while not self.pstop_hb_msgs[-1].stop:
+            self.pstop_publisher.publish(build_pstop_message(machine_uuid, self.uuid, PSTOP_PRESSED))
+            time.sleep(TEST_HEARTBEAT_TIMEOUT_S - TIMEOUT_PADDING)
+            rclpy.spin_once(self.node, timeout_sec=0.1)
+
+        self.assertTrue(self.pstop_hb_msgs[-1].stop, 'Protective stop bool not set to false')
+
+        PSTOP_PRESSED = False
+
+        while self.pstop_hb_msgs[-1].stop:
+            self.pstop_publisher.publish(build_pstop_message(machine_uuid, self.uuid, PSTOP_PRESSED))
+            time.sleep(TEST_HEARTBEAT_TIMEOUT_S - TIMEOUT_PADDING)
+            rclpy.spin_once(self.node, timeout_sec=0.1)
+
+        self.assertFalse(self.pstop_hb_msgs[-1].stop, 'Protective stop bool not set to false')
+
+        self._deactivate()
+
+    def test_exceeds_heartbeats(self):
+        self._activate()
+        self._spin_till_ok()
+
+        while not self.pstop_hb_msgs[-1].stop:
+            self.pstop_publisher.publish(build_pstop_message(machine_uuid, self.uuid, False))
+            time.sleep(TEST_HEARTBEAT_TIMEOUT_S + 1.0)
+            rclpy.spin_once(self.node, timeout_sec=0.1)
+
+        while self.pstop_msgs[-1].connection_status.status != PStopRemoteStatusEnum.UNSTABLE.value:
+            self.pstop_publisher.publish(build_pstop_message(machine_uuid, self.uuid, False))
+            time.sleep(TEST_HEARTBEAT_TIMEOUT_S + 1.0)
+            rclpy.spin_once(self.node, timeout_sec=0.1)
+
+        self._deactivate()
+
+    def test_recovers_from_unstable(self):
+        self._activate()
+        self._spin_till_ok()
+
+        while not self.pstop_hb_msgs[-1].stop:
+            self.pstop_publisher.publish(build_pstop_message(machine_uuid, self.uuid, False))
+            time.sleep(TEST_HEARTBEAT_TIMEOUT_S + 1.0)
+            rclpy.spin_once(self.node, timeout_sec=0.1)
+
+        while self.pstop_msgs[-1].connection_status.status != PStopRemoteStatusEnum.UNSTABLE.value:
+            self.pstop_publisher.publish(build_pstop_message(machine_uuid, self.uuid, False))
+            time.sleep(TEST_HEARTBEAT_TIMEOUT_S + 1.0)
+            rclpy.spin_once(self.node, timeout_sec=0.1)
+
+        self._spin_till_ok()
+        self._deactivate()
+
+    def test_deactivates_after_set_timeout(self):
+        self._activate()
+        self._spin_till_ok()
+
+        # Spin till unstable
+        while self.pstop_msgs[-1].connection_status.status != PStopRemoteStatusEnum.UNSTABLE.value:
+            self.pstop_publisher.publish(build_pstop_message(machine_uuid, self.uuid, False))
+            time.sleep(TEST_HEARTBEAT_TIMEOUT_S + 1.0)
+            rclpy.spin_once(self.node, timeout_sec=0.1)
+
+        time.sleep(TEST_DEACTIVATION_TIMEOUT_S)
+
+        # Should be able to re-activate since it has been deactivated internally
+        self._activate()
+        self._deactivate()
+
+    def test_setting_unmonitored_mode(self):
+        # Setup params client
+        self.set_params_srv = self.node.create_client(SetParameters, '/protective_stop_node/set_parameters')
+        if not self.set_params_srv.wait_for_service(timeout_sec=5.0):
+            self.fail('Service /protective_stop_node/set_parameters not available')
+
+        future = self.set_params_srv.call_async(
+            SetParameters.Request(
+                parameters=[
+                    rclpy.parameter.Parameter('is_user_monitored', rclpy.Parameter.Type.BOOL, False).to_parameter_msg()
+                ]
+            )
+        )
+        rclpy.spin_until_future_complete(self.node, future)
+
+        # Wait for a heartbeat with stop == False
+        timeout = 5.0
+        start_time = time.time()
+        while time.time() - start_time < timeout:
+            rclpy.spin_once(self.node, timeout_sec=0.1)
+            time.sleep(0.1)
+
+        self.assertFalse(self.pstop_hb_msgs[-1].stop)
+
+        # test re-enable pstop
+        future = self.set_params_srv.call_async(
+            SetParameters.Request(
+                parameters=[
+                    rclpy.parameter.Parameter('is_user_monitored', rclpy.Parameter.Type.BOOL, True).to_parameter_msg()
+                ]
+            )
+        )
+        rclpy.spin_until_future_complete(self.node, future)
+
+        # Wait for a heartbeat with stop == True
+        start_time = time.time()
+        while time.time() - start_time < timeout:
+            rclpy.spin_once(self.node, timeout_sec=0.1)
+            time.sleep(0.1)
+
+        self.assertTrue(self.pstop_hb_msgs[-1].stop)
